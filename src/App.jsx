3c7ae62246dff6ebf6c9abfaf2cb19f9e604e91e
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-03-12 02:15:41 -0400

    update wine functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react'
 import {FaPlusCircle} from 'react-icons/fa'
-import {VscSave} from 'react-icons/vsc'
+import { FaCheckCircle } from "react-icons/fa"
 import { FaMinusCircle } from "react-icons/fa"
 import { MdChangeHistory } from "react-icons/md"
 export default function App() {
   const [wines, setWines] = useState([])
-  const [editForm, setEditForm] = useState(false)
-  const [hiddenID, setHiddenID] = useState('')
   const getWines = ()=>{
     fetch('/api/wines')
       .then(res=>res.json())
@@ -42,43 +40,38 @@ export default function App() {
       .catch(err=>console.log(err))
   }
   async function updateWine(formData){
+    const bin = formData.get('bin') ? formData.get('bin') : formData.get('binPrev')
+    const description = formData.get('description') ? formData.get('description') : formData.get('descriptionPrev')
+    const vintageSize = formData.get('vintageSize') ? formData.get('vintageSize') : formData.get('vintageSizePrev')
+    const price = formData.get('price') ? formData.get('price') : formData.get('pricePrev')
     await fetch(`/api/wines/${formData.get('id')}`,{method:'PUT',
                                                     headers:{'Content-Type':'application/json'},
                                                     body: JSON.stringify({
-                                                      type:formData.get('type'),
-                                                      section:formData.get('section'),
-                                                      category:formData.get('category'),
-                                                      region:formData.get('region'),
-                                                      subRegion:formData.get('subRegion'),
-                                                      bin:formData.get('bin'),
-                                                      description:formData.get('description'),
-                                                      vintageSize:formData.get('vintageSize'),
+                                                      bin,
+                                                      description,
+                                                      vintageSize,
                                                       price:formData.get('price'),
-                                                      microsPrice:formData.get('microsPrice'),
-                                                      sequence:formData.get('sequence'),                      
                                                     })
     })
       .then(console.log('Wine Updated: ___'))
-      .then(setEditForm(false))
       .then(async()=>await getWines())
       .catch(err=>console.log(err))
   }
   return(
     <div id='wrapper'>
-      <form id='add-form' action={editForm ? updateWine : addWine}>
-        <input type='hidden' id='id' name='id' value={hiddenID} />
+      <form id='add-form' action={addWine}>
         <div id='form-row-1'>
           <label id='bin-label'>
             Bin#
-            <input type='text' maxlength='4' id='bin' name='bin' placeholder='Bin#' />
+            <input type='text' maxLength='4' id='bin' name='bin' placeholder='Bin#' />
           </label>
           <label>
             Vintage
-            <input type='text' maxlength='6' id='vintage-size' name='vintageSize' placeholder='Vintage' />
+            <input type='text' maxLength='6' id='vintage-size' name='vintageSize' placeholder='Vintage' />
           </label>
           <label>
             Price
-            <input type='text' maxlength='6' id='price' name='price' placeholder='Price' />
+            <input type='text' maxLength='6' id='price' name='price' placeholder='Price' />
           </label>
           <label>
             Typos
@@ -93,7 +86,7 @@ export default function App() {
           <label>
             Type of Wine
             <select id='type' name='type' >
-              <option disabled selected value=''>Select Wine Type...</option>
+              <option disabled defaultValue value=''>Select Type...</option>
               <option value='BY THE GLASS'>BY THE GLASS</option>
               <option value='HALF BOTTLES'>HALF BOTTLES</option>
               <option value='LARGE FORMATS'>LARGE FORMATS</option>
@@ -110,7 +103,7 @@ export default function App() {
           <label>
             Section
             <select id='section' name='section' >
-              <option disabled selected value=''>Select Wine Section...</option>
+              <option disabled defaultValue value=''>Select Section...</option>
               <option value='CHAMPAGNE'>CHAMPAGNE</option>
               <option value='WHITE WINE'>WHITE WINE</option>
               <option value='RED WINE'>RED WINE</option>
@@ -119,18 +112,18 @@ export default function App() {
           </label>
 
         </div>
-        <button style={editForm ? {background:'blue'} : {background:'green'}}>
-          {editForm ? <><VscSave /> Save Changes</> : <><FaPlusCircle /> Add Wine</>}
+        <button style={{background:'green'}}>
+          {<><FaPlusCircle /> Add Wine</>}
         </button>
       </form>
       {wines.map(data=>{
         return(
-          <>
-            <div className='wine-display' key={data._id}>
-              <span className='bin-display'>{data.bin}</span> 
-              <span className='description-display'>{data.description}</span> 
-              <span className='vintage-size-display'>{data.vintageSize}</span> 
-              <span className='price-display'>{data.price}</span>
+          <div key={data._id}>
+            <div className='wine-display'>
+              <span className='bins'><span className='bin-display'>{data.bin}</span></span>
+              <span className='descriptions'><span className='description-display'>{data.description}</span></span>
+              <span className='vintageSizes'><span className='vintage-size-display'>{data.vintageSize}</span></span> 
+              <span className='prices'><span className='price-display'>{data.price}</span></span>
               <button style={{background:'red'}} onClick={()=>deleteWine(data._id)} >
                 <FaMinusCircle /> Delete
               </button>
@@ -138,15 +131,23 @@ export default function App() {
                 <MdChangeHistory /> Edit
               </button>
             </div>
-            <div className='edit-display'>
-              <form>
-                <input type='text' name='bin' className='edit-bin' value={data.bin} />
-                <input type='text' name='description' className='edit-description' value={data.description} />
-                <input type='text' name='vintageSize' className='edit-vintage-size' value={data.vintageSize} />
-                <input type='text' name='price' className='edit-price' value={data.price} />
+            <div id={`edit-display-${data._id}`}>
+              <form action={updateWine} >
+                <input type='hidden' name='id' value={data._id} />
+                <input type='hidden' name='binPrev' value={data.bin} />
+                <input type='hidden' name='descriptionPrev' value={data.description} />
+                <input type='hidden' name='vintageSizePrev' value={data.vintageSize} />
+                <input type='hidden' name='pricePrev' value={data.price} />
+                <span className='bins'><input type='text' name='bin' className='bin-edit' placeholder={data.bin} /></span>
+                <span className='descriptions'><input type='text' name='description' className='description-edit' placeholder={data.description} /></span>
+                <span className='vintageSizes'><input type='text' name='vintageSize' className='vintage-size-edit' placeholder={data.vintageSize} /></span>
+                <span className='prices'><input type='text' name='price' className='price-edit' placeholder={data.price} /></span>
+                <button className='save-changes' style={{background:'blue'}} >
+                  <FaCheckCircle />  &nbsp;Save Changes
+                </button>
               </form>              
             </div>
-          </>
+          </div>
         )
       })}
     </div>
