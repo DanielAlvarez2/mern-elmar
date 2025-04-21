5dfde7a9c8dfc478d00b80d6fb575a952438348c
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-04-21 11:09:23 -0400

    add margin to #type-of-wine in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -147,23 +147,23 @@ export default function App() {
                     <span className='prices'><input style={{color:'blue'}} type='text' name='price' className='price-edit' defaultValue={data.price} /></span>
                   </div>
                   <div style={{display:'flex'}}>
-                  <label>
-                Type of Wine 
-                <select id='type' name='type' defaultValue = {data.type} key={Date.now()} >
-                    <option disabled value=''>Select Type...</option>
-                    <option value='BY THE GLASS'>BY THE GLASS</option>
-                    <option value='HALF BOTTLES'>HALF BOTTLES</option>
-                    <option value='LARGE FORMATS'>LARGE FORMATS</option>
-                    <option value='SAKE'>SAKE</option>
-                    <option value='SPARKLING'>SPARKLING</option>
-                    <option value='CHAMPAGNE'>CHAMPAGNE</option>
-                    <option value='WHITE WINE'>WHITE WINE</option>
-                    <option value='ROSÉ WINE'>ROSÉ WINE</option>
-                    <option value='RED WINE'>RED WINE</option>
-                    <option value='SWEET WINE'>SWEET WINE</option>
-                    <option value='FORTIFIED WINE'>FORTIFIED WINE</option>
-                </select>
-            </label>   
+                  <label id='type-of-wine'>
+                    Type of Wine 
+                    <select id='type' name='type' defaultValue = {data.type} key={Date.now()} >
+                        <option disabled value=''>Select Type...</option>
+                        <option value='BY THE GLASS'>BY THE GLASS</option>
+                        <option value='HALF BOTTLES'>HALF BOTTLES</option>
+                        <option value='LARGE FORMATS'>LARGE FORMATS</option>
+                        <option value='SAKE'>SAKE</option>
+                        <option value='SPARKLING'>SPARKLING</option>
+                        <option value='CHAMPAGNE'>CHAMPAGNE</option>
+                        <option value='WHITE WINE'>WHITE WINE</option>
+                        <option value='ROSÉ WINE'>ROSÉ WINE</option>
+                        <option value='RED WINE'>RED WINE</option>
+                        <option value='SWEET WINE'>SWEET WINE</option>
+                        <option value='FORTIFIED WINE'>FORTIFIED WINE</option>
+                    </select>
+                  </label>   
                   </div>
                   <div style={{marginBottom:'100px',display:'flex'}}>
                     <button className='save-changes' style={{background:'blue'}} >
